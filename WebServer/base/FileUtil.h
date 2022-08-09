3344ee8b883abcedf29c2d5347b447c82ba02bc2
hyc <hyc@bytedance.com>
2022-08-09 20:23:57 +0800

    init

---
+++ WebServer/base/FileUtil.h
@@ -0,0 +1,20 @@
+// @Author yichen he
+// @Email hyc2026@yeah.net
+#pragma once
+#include <string>
+#include "noncopyable.h"
+
+
+class AppendFile : noncopyable {
+ public:
+  explicit AppendFile(std::string filename);
+  ~AppendFile();
+  // append 会向文件写
+  void append(const char *logline, const size_t len);
+  void flush();
+
+ private:
+  size_t write(const char *logline, size_t len);
+  FILE *fp_;
+  char buffer_[64 * 1024];
+};
---
+++ WebServer/base/noncopyable.h
@@ -0,0 +1,13 @@
+// @Author yichen he
+// @Email hyc2026@yeah.net
+#pragma once
+
+class noncopyable {
+ protected:
+  noncopyable() {}
+  ~noncopyable() {}
+
+ private:
+  noncopyable(const noncopyable&);
+  const noncopyable& operator=(const noncopyable&);
+};
